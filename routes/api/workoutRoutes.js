c069b9d560dce7b4a63284911f1c7c4b6cbbfce3
jagg1991 <josephguajardo@ymail.com>
2021-04-19 12:39:02 -0700

    adding to models folder

---
+++ routes/api/workoutRoutes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const { Workout } = require("../../models");
+
+router.get('/', (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+});
+
+module.exports = router;
